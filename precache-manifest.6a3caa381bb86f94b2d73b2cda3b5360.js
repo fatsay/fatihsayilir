26d396a5a34a0eddf87f50eb22cb013c1bea0026
fatsay <fatih_sayilir@hotmail.com>
2020-10-11 18:09:30 +0200

    Updates

---
+++ precache-manifest.6a3caa381bb86f94b2d73b2cda3b5360.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16ed4d29b0e47583c67640ef2f5dc6a6",
+    "url": "/fatihsayilir/index.html"
+  },
+  {
+    "revision": "8200080d205741f35258",
+    "url": "/fatihsayilir/static/css/main.fb9aaa10.chunk.css"
+  },
+  {
+    "revision": "e95b35784a92bc292abd",
+    "url": "/fatihsayilir/static/js/2.ad28f8b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fatihsayilir/static/js/2.ad28f8b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8200080d205741f35258",
+    "url": "/fatihsayilir/static/js/main.f5d0254c.chunk.js"
+  },
+  {
+    "revision": "95bcfc365d728a4a94f6",
+    "url": "/fatihsayilir/static/js/runtime-main.e3ebf466.js"
+  },
+  {
+    "revision": "b5a8bd4198cf60433241757fd693a80a",
+    "url": "/fatihsayilir/static/media/FATIH_SAYILIR-CV.b5a8bd41.pdf"
+  },
+  {
+    "revision": "d45a2e4fd7aca4f8e9227ec8e30be5c6",
+    "url": "/fatihsayilir/static/media/android.d45a2e4f.png"
+  },
+  {
+    "revision": "0995e47f2ddee183e0a02cc34ef0461b",
+    "url": "/fatihsayilir/static/media/profile.0995e47f.png"
+  },
+  {
+    "revision": "6571d1529be0dd6ae440e4f44135efb8",
+    "url": "/fatihsayilir/static/media/web-design.6571d152.png"
+  }
+]);
